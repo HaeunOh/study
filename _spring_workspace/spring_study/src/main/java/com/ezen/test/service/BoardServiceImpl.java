f0869157cb99a1a816604d51f9de77cb97443957
haeunOh <25_asa@naver.com>
2024-04-24 18:19:30 +0900

    2024-04-24

---
+++ _spring_workspace/spring_study/src/main/java/com/ezen/test/service/BoardServiceImpl.java
@@ -0,0 +1,51 @@
+package com.ezen.test.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ezen.test.domain.BoardVO;
+import com.ezen.test.repository.BoardDAO;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class BoardServiceImpl implements BoardService{
+	
+	@Autowired(required=true)
+	private BoardDAO bdao;
+
+	@Override
+	public int insert(BoardVO bvo) {
+		log.info(">>> board insert service ok");
+		return bdao.insert(bvo);
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		log.info(">>> board getList service ok");
+		return bdao.getList();
+	}
+
+	@Override
+	public BoardVO getDetail(int bno) {
+		log.info(">>> board getDetail service ok");
+		bdao.updateReadCount(bno);
+		return bdao.getDetail(bno);
+	}
+
+	@Override
+	public void update(BoardVO bvo) {
+		log.info(">>> board update service ok");
+		bdao.update(bvo);
+	}
+
+	@Override
+	public void remove(BoardVO bvo) {
+		log.info(">>> board remove service ok");
+		bdao.remove(bvo);
+		
+	}
+}
